f4ebda084ff9f5ee45c869ad9720a9e1ff883df0
kelvinhuang98 <kelvin.huang98@hotmail.com>
2022-10-27 18:35:34 -0400

    Finalized function to retrieve ISS fly over times of an area based on its coordinates

--- index.js
+++ index.js
@@ -1,31 +1,50 @@
-const { fetchMyIP, fetchCoordsByIP, fetchISSFlyOverTimes } = require("./iss");
+// const { fetchMyIP, fetchCoordsByIP, fetchISSFlyOverTimes } = require("./iss");
 
-fetchMyIP((error, ip) => {
+// fetchMyIP((error, ip) => {
+//   if (error) {
+//     console.log("It didn't work!", error);
+//     return;
+//   }
+
+//   console.log("It worked! Returned IP:", ip);
+// });
+
+// fetchCoordsByIP("142.126.69.192", (error, data) => {
+//   if (error) {
+//     console.log("It didn't work!", error);
+//     return;
+//   }
+
+//   console.log("It worked! Returned coordinates:", data);
+// });
+
+// fetchISSFlyOverTimes(
+//   { latitude: 43.653226, longitude: -79.3831843 },
+//   (error, data) => {
+//     if (error) {
+//       console.log("It didn't work!", error);
+//       return;
+//     }
+
+//     console.log("It worked! Returned flyover times:", data);
+//   }
+// );
+
+const { nextISSTimesForMyLocation } = require("./iss");
+
+const printPassTimes = (passTimes) => {
+  for (const passTime of passTimes) {
+    const datetime = new Date(0);
+    datetime.setUTCSeconds(passTime.risetime);
+    const duration = passTime.duration;
+    console.log(`Next pass at ${datetime} for ${duration} seconds!`);
+  }
+};
+
+nextISSTimesForMyLocation((error, passTimes) => {
   if (error) {
-    console.log("It didn't work!", error);
-    return;
+    return console.log("It didn't work!", error);
   }
-
-  console.log("It worked! Returned IP:", ip);
+  // success, print out the deets!
+  printPassTimes(passTimes);
 });
-
-fetchCoordsByIP("142.126.69.192", (error, data) => {
-  if (error) {
-    console.log("It didn't work!", error);
-    return;
-  }
-
-  console.log("It worked! Returned coordinates:", data);
-});
-
-fetchISSFlyOverTimes(
-  { latitude: 43.653226, longitude: -79.3831843 },
-  (error, data) => {
-    if (error) {
-      console.log("It didn't work!", error);
-      return;
-    }
-
-    console.log("It worked! Returned flyover times:", data);
-  }
-);
